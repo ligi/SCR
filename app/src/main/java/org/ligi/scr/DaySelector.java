0ab0455586e6f08c032808ec21c203730c2a75fb
ligi <ligi@ligi.de>
2015-11-25 17:27:02 +0100

    Simplify

--- app/src/main/java/org/ligi/scr/DaySelector.java
+++ app/src/main/java/org/ligi/scr/DaySelector.java
@@ -4,23 +4,19 @@ import android.content.Context;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-import com.google.common.base.Function;
-import com.google.common.collect.Lists;
-
 import org.ligi.scr.model.Day;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DaySelector extends Spinner {
     public DaySelector(Context context) {
         super(context);
 
-        final List<String> res = Lists.transform(App.conference.days, new Function<Day, String>() {
-            @Override
-            public String apply(Day input) {
-                return "#" + App.conference.days.indexOf(input) + " " + input.date;
-            }
-        });
+        final List<String> res = new ArrayList<>();
+        for (Day day : App.conference.days) {
+            res.add("#" + App.conference.days.indexOf(day) + " " + day.date);
+        }
 
         final ArrayAdapter<String> adapter = new ArrayAdapter<>(context,
                 R.layout.day_spinner_item, android.R.id.text1,
