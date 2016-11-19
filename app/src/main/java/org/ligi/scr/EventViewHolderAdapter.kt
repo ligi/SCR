1408bd726a2b3d92428d1f2c5508371dc0db8f12
ligi <ligi@ligi.de>
2016-11-19 20:00:47 +0100

    Simplify

--- app/src/main/java/org/ligi/scr/EventViewHolderAdapter.kt
+++ app/src/main/java/org/ligi/scr/EventViewHolderAdapter.kt
@@ -6,18 +6,15 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import info.metadude.java.library.halfnarp.model.GetTalksResponse
 
-internal class EventViewHolderAdapter(private val talkResponses: List<GetTalksResponse>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+internal class EventViewHolderAdapter(private val talkResponses: List<GetTalksResponse>) : RecyclerView.Adapter<EventViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val res = inflater.inflate(R.layout.item_event, parent, false) as CardView
         return EventViewHolder(res)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as EventViewHolder).apply(talkResponses[position])
-
-    }
+    override fun onBindViewHolder(holder: EventViewHolder, position: Int) = holder.apply(talkResponses[position])
 
     override fun getItemCount() = talkResponses.size
 
